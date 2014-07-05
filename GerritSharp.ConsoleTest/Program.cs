e7c67165683a00f4808891d5bc8b64372cb6c2e0
Daryl Walleck <daryl.walleck@rackspace.com>
2014-07-04 22:53:21 -0500

    More work on changeInfo

---
+++ GerritSharp.ConsoleTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using GerritSharp.Clients;
+using GerritSharp.Models.Changes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerritSharp.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ChangesClient("https://review.openstack.org");
+            var changes = client.GetChanges().Result;
+            foreach (ChangeInfo change in changes)
+            {
+                Console.WriteLine(change.Subject);
+            }
+
+        }
+    }
+}
--- GerritSharp/Models/Changes/ChangeInfo.cs
+++ GerritSharp/Models/Changes/ChangeInfo.cs
@@ -12,49 +12,115 @@ namespace GerritSharp.Models.Changes
         [JsonProperty(PropertyName = "kind")]
         public string Kind { get; set; }
 
-        public string id { get; set; }
-        public string project { get; set; }
-        public string branch { get; set; }
-        public string change_id { get; set; }
-        public string subject { get; set; }
-        public string status { get; set; }
-        public string created { get; set; }
-        public string updated { get; set; }
-        public bool mergeable { get; set; }
-        public Owner owner { get; set; }
-        public Labels labels { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "project")]
+        public string Project { get; set; }
+
+        [JsonProperty(PropertyName = "branch")]
+        public string Branch { get; set; }
+
+        [JsonProperty(PropertyName = "change_id")]
+        public string ChangeId { get; set; }
+
+        [JsonProperty(PropertyName = "subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+        [JsonProperty(PropertyName = "created")]
+        public string Created { get; set; }
+
+        [JsonProperty(PropertyName = "updated")]
+        public string Updated { get; set; }
+
+        [JsonProperty(PropertyName = "mergeable")]
+        public bool IsMergeable { get; set; }
+
+        [JsonProperty(PropertyName = "owner")]
+        public User Owner { get; set; }
+
+        [JsonProperty(PropertyName = "labels")]
+        public Labels Labels { get; set; }
+
+        [JsonProperty(PropertyName = "_number")]
+        public string Number { get; set; }
+
+        [JsonProperty(PropertyName = "_sortkey")]
+        public string SortKey { get; set; }
     }
 
-
-    public class Owner
+    public class User
     {
-        public string name { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "_account_id")]
+        public string AccountId { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
     }
+
+    public class UserReview : User
+    {
+        [JsonProperty(PropertyName = "value")]
+        public string Value{ get; set; }
+    }
+
 
     public class Labels
     {
         public Verified Verified { get; set; }
+
+        [JsonProperty(PropertyName = "Code-Review")]
         public CodeReview CodeReview { get; set; }
+        
         public Workflow Workflow { get; set; }
     }
 
     public class Verified
     {
+        public Recommended recommended { get; set; }
         public Disliked disliked { get; set; }
+        public Approved approved { get; set; }
         public int value { get; set; }
     }
 
-    public class Disliked
+    public class Recommended : UserReview
     {
-        public string name { get; set; }
+
+    }
+
+    public class Disliked : UserReview
+    {
+        
+    }
+
+    public class Approved : UserReview
+    {
+
     }
 
     public class CodeReview
     {
+        public Recommended recommended { get; set; }
+        public Disliked disliked { get; set; }
+        public Approved approved { get; set; }
+        public int value { get; set; }
     }
 
     public class Workflow
     {
+        public Recommended recommended { get; set; }
+        public Disliked disliked { get; set; }
+        public Approved approved { get; set; }
+        public int value { get; set; }
     }
 
 }
